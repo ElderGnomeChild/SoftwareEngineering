4d6e55b359e773c183ee0194ae41e0294f3d19dd
ElderGnomeChild <daniellier12@gmail.com>
2019-04-17 10:22:18 -0600

    beautified the website

--- Django_Site/please_work/settings.py
+++ Django_Site/please_work/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',
+    # 'accounts.apps.AccountsConfig',
     'users.apps.UsersConfig'
 ]
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
